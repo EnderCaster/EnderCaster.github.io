fad1a167a225b26a4974e39e99f4e139dc257b18
EnderCaster <mstext0@hotmail.com>
2018-03-19 20:36:35 +0800

    update powershell note

--- commit.sh
+++ commit.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 git commit -a
 git push
+jekyll build
 cd _site
 git add -A
 git commit -a
--- get-jekyll-template.sh
+++ get-jekyll-template.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+if [[  $2 == "" ]]
+then
+	echo 'command <title> <filename>'
+	exit 1;
+fi
 filename=$(date +"%Y-%m-%d")
 header="---\n"
 header=$header"layout: post\n"
 if [[ $1 ]]
 then
 header=$header"title: \""$1"\"\n"
-filename=$filename"-"${1// /-}
 else
 header=$header"title:\n"
 fi
@@ -13,9 +17,5 @@ header=$header"date: "$(date +"%Y-%m-%d %H:%M:%S %:z")"\n"
 header=$header"categories: \n- CS\n"
 header=$header"tags: \n- \"Computer Science\"\n"
 header=$header"---"
-if [[ $1 ]]
-then
-	echo -e $header > "_posts/"$filename".markdown"
-else
-	echo -e $header""
-fi 
+filename=$filename"-"${2// /-}
+echo -e $header > "_posts/"$filename".markdown"
